1328c71bc09047ea7f60143c41ad8c0aae61f800
jesk5603 <jesk5603@gmail.com>
2022-09-17 15:44:12 +0800

    作業

---
+++ class1/HW/HW.py
@@ -0,0 +1,19 @@
+'''
+請將下列變數做出正確的型態分類
+apple = 1000
+age = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+整數int  :
+布林bool :
+字串str  :
+浮點數float:
+example:
+ball = 100
+talk = 'hello'
+則要在型態列表中填入該變數
+int:ball
+str:talk
+'''
