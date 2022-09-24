0bdfcfd2a81c6f9844ec96e7702293f75e42fb88
jesk5603 <jesk5603@gmail.com>
2022-09-24 15:31:12 +0800

    hw

---
+++ class2/class/class.py
@@ -0,0 +1,19 @@
+'''
+max(找出最大值)
+type(分辨資料)
+len(分辨長度(字串))
+int(轉換成整數)
+str(轉成字串)
+float(轉成浮點數)
+bool(轉布林值)不管inut輸入什麼型態都是字串
+'''
+# print ('請輸入數字')
+# a= input ()
+# print('輸入的數字是')
+# print(a)
+# a= input ('請輸入你的年齡:')
+# print('你的年齡是:'+a+'歲歲')
+p=3.14
+r=int(input('請輸入半徑:'))
+a=p*r**2
+print('圓面積是', a)
