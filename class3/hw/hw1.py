ce328c17456f3bb0c90109079956b07a66b1ce60
jesk5603 <jesk5603@gmail.com>
2022-10-01 15:35:18 +0800

    hw

--- class3/hw/hw1.py
+++ class3/hw/hw1.py
@@ -1,5 +1,9 @@
 #  5/9 乘(華氏溫度- 32)
 try:
-    f=float(input('請輸入華氏溫度:')) 
+    f = float(input('請輸入華氏溫度:'))
 except:
     print('輸入錯誤')
+else:
+    c = 5 / 9 * (f - 32)
+    print(f'華氏溫度{f}F等於攝氏溫度{c}C')
+print('程式結束')
